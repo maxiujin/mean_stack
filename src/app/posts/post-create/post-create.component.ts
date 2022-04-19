4e856978d87e6ecdb0a7155fc1f4ed891533c69b
matt thompkins <alkeccg@gmail.com>
2022-04-18 21:22:37 -0700

    first commit

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-post-create',
+  templateUrl:'./post-create.component.html',
+
+})
+
+export class PostCreateComponent {
+newPost = 'NO content';
+comment:string ='';
+onAddPost(){
+  this.newPost = 'The user\'s post';
+}
+
+}
